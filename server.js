e4888dc339f8b2022823436b23eab328ee35fc53
sbahri7 <satyambahri12345@gmail.com>
2025-07-20 20:29:42 -0400

    Restructure for Vercel Node.js serverless deploy

--- server.js
+++ server.js
@@ -9,7 +9,7 @@
 *  Name: Satyam Bahri   Student ID: 172151227   Date: 2025-07-14
 *
 *  Published URL: https://web-app322-git-main-satyam-bahris-projects.vercel.app
-*
+*git URl : https://github.com/sbahri7/WEB322.git
 ********************************************************************************/
 
 
